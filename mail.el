43a87f6325754ce8c4201f364807300ca552efbb
Moritz Wilhelmy <mw@barfooze.de>
2018-06-17 23:01:07 +0200

    Archive mu4e mails by year

--- mail.el
+++ mail.el
@@ -1,17 +1,20 @@
-(custom-set-variables
- '(mu4e-auto-retrieve-keys t)
- '(mu4e-change-filenames-when-moving t)
- '(mu4e-compose-in-new-frame t)
- '(mu4e-drafts-folder "/.Drafts")
- '(mu4e-get-mail-command "mbsync -a")
- '(mu4e-refile-folder "/.Archive.mu4e")
- '(mu4e-sent-folder "/.Sent")
- '(mu4e-trash-folder "/.Trash")
- '(mu4e-use-fancy-chars nil)
+(let ((mu4e-archive (concat "/.Archive." (format-time-string "%Y"))))
+  (custom-set-variables
+   `(mu4e-refile-folder ,mu4e-archive) ; archive mail by year
+   '(mu4e-auto-retrieve-keys t)
+   '(mu4e-change-filenames-when-moving t)
+   '(mu4e-compose-in-new-frame t)
+   '(mu4e-drafts-folder "/.Drafts")
+   '(mu4e-get-mail-command "mbsync -a")
+   '(mu4e-sent-folder "/.Sent")
+   '(mu4e-trash-folder "/.Trash")
+   '(mu4e-use-fancy-chars nil)
 
- '(message-send-mail-function (quote message-send-mail-with-sendmail))
- ;'(send-mail-function (quote mailclient-send-it))
- '(user-full-name "Moritz Wilhelmy")
- )
+   '(message-send-mail-function (quote message-send-mail-with-sendmail))
+   ;;'(send-mail-function (quote mailclient-send-it))
+   '(user-full-name "Moritz Wilhelmy")
+ ))
 
-(try-loading-file "~/.spacemacs.d/personal.el") ; defines mu4e-maildir and mail-host-address 
+;; defines mu4e-maildir and mail-host-address, which are none of your business.
+;; I might put something more secret in there, I guess, so it's nice to have.
+(try-loading-file "~/.spacemacs.d/personal.el") 
